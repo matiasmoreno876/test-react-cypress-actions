5ab818240c11bcb23f056d6a72f99d33ab4795b5
Matias Ezequiel Moreno <matiasmoreno876@gmail.com>
2021-06-11 14:38:33 -0300

    added config for reports

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,15 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import addContext from 'mochawesome/addContext'
 
+Cypress.on("test:after:run", (test, runnable) => {
+
+    let videoName = Cypress.spec.name
+    videoName = videoName.replace('/.js.*', '.js')
+    const videoUrl = 'videos/' + videoName + '.mp4'
+
+    addContext({ test }, videoUrl)
+});
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
