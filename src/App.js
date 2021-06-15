08f276f2adb2e463c34a413878c382c6005ba5d7
Matias Ezequiel Moreno <matiasmoreno876@gmail.com>
2021-06-15 09:08:59 -0300

    test number 2

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-        <h1>Esto es una prueba</h1>
+        <h1>Esto es una prueba numero 2</h1>
       </header>
     </div>
   );
